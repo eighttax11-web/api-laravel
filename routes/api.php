d5af3000958e92dedb5906cb86f94595323a1de1
Cristian <eighttax11@gmail.com>
2021-03-26 16:55:37 -0600

    Agregamos la librería Laravel Sanctum y se protegieron las rutas de la API versión uno y dos

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 // Version 1
 Route::apiResource('v1/posts', PostV1::class)
-    ->only(['index', 'show', 'destroy']);
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
 
 // Version 2
 Route::apiResource('v2/posts', PostV2::class)
-    ->only(['index', 'show', 'destroy']);
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
