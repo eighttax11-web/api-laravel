c50dccf7c8d6319363e762443ffff78d794ef14b
Cristian <eighttax11@gmail.com>
2021-03-26 17:38:34 -0600

    Se implemento el inicio de sesión mediante tokens en la API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
+use App\Http\Controllers\Api\LoginController;
 
 // Version 1
 Route::apiResource('v1/posts', PostV1::class)
@@ -14,3 +15,9 @@ Route::apiResource('v1/posts', PostV1::class)
 Route::apiResource('v2/posts', PostV2::class)
     ->only(['index', 'show', 'destroy'])
     ->middleware('auth:sanctum');
+
+// Authentication
+Route::post('login', [
+    LoginController::class,
+    'login'
+]);
